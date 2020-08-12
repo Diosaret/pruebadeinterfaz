df0a087366cf46a476e52fc45cf805bbfbdb4bde
Marycielo <marycielooberto236@gmail.com>
2020-08-12 09:42:29 -0300

    Modifique la vista del menu en movil

--- js/menu.js
+++ js/menu.js
@@ -1,7 +1,14 @@
 var btnMenu = document.getElementById('btn-menu');
 var nav = document.getElementById('nav');
+var title1 = document.getElementById('title');
+
+
 
 btnMenu.addEventListener('click', function(){
 nav.classList.toggle('mostrar');
+title1.classList.toggle('title1');
+btnMenu.classList.toggle('menu1');
+
 
 })
+
